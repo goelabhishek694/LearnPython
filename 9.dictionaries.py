a1fbf259a2b81d816f84a5075265cb8a45140962
goelabhishek694 <goelabhishek694@gmail.com>
2021-12-08 09:31:13 +0530

    in keyword in a dictionary

--- 9.dictionaries.py
+++ 9.dictionaries.py
@@ -21,3 +21,13 @@ for student in student_attendance:
 for student, attendance in student_attendance.items():
     print(f"{student}:{attendance}")
 
+if "Bob" in student_attendance:
+    print(student_attendance["Bob"])
+else:
+    print("Bob is not a student in this class")
+
+# attendance_values will contain list of these values 
+attendance_values=student_attendance.values()
+print(attendance_values) #dict_values([90, 70, 96])
+
+# avg attendanceprint(sum(attendance_values)/len(attendance_values))
